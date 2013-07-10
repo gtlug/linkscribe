eb1ffb60b12d8d7decada7cdfe84c7fb21379147
Carl Blakemore <cblake777@gmail.com>
2013-07-10 12:54:56 -0400

    commit WiP - start of add plugins

--- LS/Command.pm
+++ LS/Command.pm
@@ -2,7 +2,10 @@ package LS::Command;
 use Mouse;
 with qw(LS::Base LS::Cache);
 
-sub getPlugins {
+##This should only be here until Module::Pluggable is setup
+use LS::Command::Example;
+
+sub refreshPlugins {
 	my $self = shift;
 	## Find all plugins in LS::Commands::*
 	
@@ -13,6 +16,15 @@ sub getPlugins {
 	## -> to keep from being too much of a burden, cache the response.
 	
 	## Caching should be a role.  Probably useful in parser as well.
+	$self->{plugins} = {};
+	foreach my $plugin (qw/LS::Command::Example/) {
+		my $pluginObj = ${plugin}->new();
+		#$plugins->{$pluginObj->{command}} = $pluginObj;
+	}
+}
+
+sub plugins {
+	#$self->{} = 
 }
 
 __PACKAGE__->meta->make_immutable;
--- t/lib/LS/Command/Test.pm
+++ t/lib/LS/Command/Test.pm
@@ -4,18 +4,26 @@ use base qw(LS::Test::Class::Base);
 use LS::Test::Class;
 use LS::Command;
 
-sub setup:Test( startup=>1 ) {
+sub setup:Test( startup ) {
 	diag("here we go.");
-	is(1,1,"yup. 1 == 1");
+	
+	#cmp_deeply(\@b, subbagof(@a), '@b is a subset of @a');
 }
 
-sub cache:Test(3) {
+sub cache:Test(2) {
+	my $self = shift;
+	
 	my $command = LS::Command->new();
 	lives_ok { $command->setCache('test','a string')} "Successfully saved data to cache";
 	is ($command->getCache('test'), 'a string', "Successfully retrieved data from cache");
-	my @a = ('a', 'b', 'c', 'd');
-	my @b = ('b', 'd');
-	cmp_deeply(\@b, subbagof(@a), '@b is a subset of @a');
 }
+
+#sub getPlugins:Test(1) {
+#	my $self = shift;
+	
+	#my $command = LS::Command->new();
+	
+	#my $plugins = $command->refreshPlugins();
+#}
 
 1;
